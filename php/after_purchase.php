2cb81bdcb5e923b5ae5ec4a027100fdc2ae52c04
artmtt <aaaajjjj72@gmail.com>
2024-04-17 00:55:50 -0600

    Reduced redirect time

--- php/after_purchase.php
+++ php/after_purchase.php
@@ -19,7 +19,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Redirect -->
-        <meta http-equiv="refresh" content="3;url=./home.php">
+        <meta http-equiv="refresh" content="2;url=./home.php">
         <title>Thank you</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link href="../css/styles.css" rel="stylesheet">
